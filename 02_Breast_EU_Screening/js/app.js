825b6937f385db307c622c682ce12fe35d82af55
LAM Frédéric <flaaam@gmail.com>
2016-06-23 16:18:55 +0200

    New map regions

--- 02_Breast_EU_Screening/js/app.js
+++ 02_Breast_EU_Screening/js/app.js
@@ -26,11 +26,50 @@
 
 	var key_val = 'PERCENT_T' ; 
 
+    var sweden_regions = {
+        0 : { 'name' : 'Norra sjukvårdsregionen' } , 
+        1 : { 'name' : 'Uppsala-Örebro sjukvårdsregion' } , 
+        2 : { 'name' : 'Stockholms sjukvårdsregion' } , 
+        3 : { 'name' : 'Sydöstra sjukvårdsregionen' } , 
+        4 : { 'name' : 'Västra sjukvårdsregionen' } , 
+        5 : { 'name' : 'Södra sjukvårdsregionen' }
+    } ; 
+
+    var sweden_counties = {
+        0 : { 'code' : 'K' , 'name' : 'Blekinge County' , 'region' : 5 } ,
+        1 : { 'code' : 'W' , 'name' : 'Dalarna County', 'region' : 1 } ,
+        2 : { 'code' : 'X' , 'name' : 'Gävleborg County', 'region' : 1 } ,
+        3 : { 'code' : 'I' , 'name' : 'Gotland County', 'region' : 2 } ,
+        4 : { 'code' : 'N' , 'name' : 'Halland County', 'region' : 4 } ,
+        5 : { 'code' : 'Z' , 'name' : 'Jämtland County ', 'region' : 0 } ,
+        6 : { 'code' : 'F' , 'name' : 'Jönköping County', 'region' : 3 } ,
+        7 : { 'code' : 'H' , 'name' : 'Kalmar County', 'region' : 3 } , 
+        8 : { 'code' : 'G' , 'name' : 'Kronoberg County', 'region' : 5 } , 
+        9 : { 'code' : 'BD' , 'name' : 'Norrbotten County', 'region' : 0 } ,
+        10 : { 'code' : 'T' , 'name' : 'Örebro County', 'region' : 1 } , 
+        11 : { 'code' : 'E' , 'name' : 'Östergötland County', 'region' : 3 } , 
+        12 : { 'code' : 'M' , 'name' : 'Skåne County', 'region' : 5 } , 
+        13 : { 'code' : 'D' , 'name' : 'Södermanland County', 'region' : 1 } , 
+        14 : { 'code' : 'AB' , 'name' : 'Stockholm County' , 'region' : 3 } , 
+        15 : { 'code' : 'C' , 'name' : 'Uppsala County', 'region' : 1 } , 
+        16 : { 'code' : 'S' , 'name' : 'Värmlands County', 'region' : 1 } ,  
+        17 : { 'code' : 'AC' , 'name' : 'Västerbotten County', 'region' : 0 } , 
+        18 : { 'code' : 'Y' , 'name' : 'Västernorrland County', 'region' : 0 } , 
+        19 : { 'code' : 'U' , 'name' : 'Västmanland County', 'region' : 1 } , 
+        20 : { 'code' : 'O' , 'name' : 'Västra Götaland County', 'region' : 4 } 
+    } ; 
+
 	queue()
 		.defer( d3.json , "data/europe.topojson" )
 	    .defer( d3.csv , "data/coverage-breast-cancer-screening.csv" )
-	    .await( function( error , topology , data ) { 
+        .defer( d3.json , "data/sweden-counties.topojson" )
+        .defer( d3.json , "data/uk.topojson" )
+        .defer( d3.json , "data/belgium.json" )
+        .defer( d3.json , "data/portugal.json")
+	    .await( function( error , europe , data , sweden , uk , belgium , portugal ) { 
     	
+        // console.info( topology.objects ) ; 
+
     	var range_colors  = Array.prototype.slice.call( colorbrewer[ 'PuRd' ][ 9 ] ) ;	
     	range_colors.unshift('#ffffff') ;
     	// range_colors[0] = '#ffffff' ;
@@ -40,9 +79,10 @@
 		    .range( range_colors ) ;
 
     	var g_general = svg.append("g").attr('class','general') ;
+
 	    
 	    g_general.selectAll("path")
-	        .data( topojson.object(topology, topology.objects.europe ).geometries )
+	        .data( topojson.object( europe, europe.objects.collection ).geometries )
 	        .enter()
 	        .append("path")
 	        .attr("d", path)
@@ -61,11 +101,144 @@
 	        	for( var item in data )
 	        		if ( data[item].CODE == d.id )
 	        			// console.info( d.id ,  data[item][key_val] , quantize(data[item][key_val]) );
-	        			return quantize(data[item][key_val]);
+	        			return quantize( data[item][key_val] );
 	        	return "#ffffff" ; 
 	        })
-	        .attr('title',function(d){ return d.properties.NAME ; })
+	        .attr('title',function(d){ 
+                if ( d.properties == undefined ) return ; 
+                return d.properties.NAME ; 
+            })
 	    ;
+
+        var g_sweden = svg.append("g").attr('class','sweden') ;
+        g_sweden.selectAll("path")
+            .data( topojson.object( sweden , sweden.objects.SWE_adm1 ).geometries )
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr('class',function(d,i){
+
+                return "swe_counties " + sweden_counties[i].code  ; 
+            })
+            .attr('fill',function(d,i){
+                var id_region = sweden_counties[i].region ;
+
+                for( var item in data )
+                {
+                    if ( data[item].CODE == 'SE' )
+                    {
+                        if ( data[item].CODE_REGION == id_region )
+                        {
+                            return quantize( data[item][key_val] );
+                        }
+                    }
+                }
+
+                return "#ffffff" ; 
+            })
+            .attr('stroke',function(d,i){
+                var id_region = sweden_counties[i].region ;
+
+                for( var item in data )
+                {
+                    if ( data[item].CODE == 'SE' )
+                    {
+                        if ( data[item].CODE_REGION == id_region )
+                        {
+                            return quantize( data[item][key_val] );
+                        }
+                    }
+                }
+
+                return "transparent" ; 
+            })
+        ;
+
+        /*var g_sweden_regions = svg.append("g").attr('class','sweden') ;
+        console.info( sweden_regions.objects.regions ) ; 
+        g_sweden_regions.selectAll("path")
+            .data( topojson.object( sweden_regions , sweden_regions.objects.regions ).geometries )
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr('class',function(d,i){
+                return "swe_regions"  ; 
+            })
+            .attr('fill',function(d,i){
+                return "pink" ; 
+            })
+        ;*/
+
+
+        var g_belgium = svg.append("g").attr('class','belgium') ;
+        
+        g_belgium.selectAll("path")
+            .data( topojson.object( belgium , belgium.objects.belgium ).geometries )
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr('class',function(d,i){
+                return "belgium_regions region-be-" + i    ; 
+            })
+            .attr('fill',function(d,i){
+
+                for( var item in data )
+                {
+                    if ( data[item].CODE == 'BE' )
+                    {
+                        console.info( data[item] , data[item].CODE_REGION, i ) ; 
+                        if ( data[item].CODE_REGION == i )
+                        {
+                            return quantize( data[item][key_val] );
+                        }
+                    }
+                }
+            })
+        ;
+
+
+        var g_portugal = svg.append("g").attr('class','portugal') ;
+        
+        g_portugal.selectAll("path")
+            .data( topojson.object( portugal , portugal.objects.portugal ).geometries )
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr('class',function(d,i){
+                return "portugal_regions "   ; 
+            })
+            .attr('fill',function(d,i){
+                return "blue" ; 
+            })
+        ;
+
+        var g_uk = svg.append("g").attr('class','uk') ;
+        
+        g_uk.selectAll("path")
+            .data( topojson.object( uk , uk.objects.subunits ).geometries )
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr('class',function(d,i){
+                return "uk_subunit subunit-" + d.id  ; 
+            })
+            .attr('fill',function(d,i){
+                var id_subunit = d.id ;
+
+                for( var item in data )
+                {
+                    if ( data[item].CODE == 'GB' )
+                    {
+                        if ( data[item].CODE_REGION == id_subunit )
+                        {
+                            return quantize( data[item][key_val] );
+                        }
+                    }
+                }
+
+                return "#ffffff" ; 
+            })
+        ;
 
 	    /*var label_regions = svg.selectAll(".place-label")
 		    .data(topojson.object(topology, topology.objects.europe ).geometries)
